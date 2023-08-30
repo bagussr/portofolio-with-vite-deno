5b621820ad7e66e6331eec835538ff6a18632221
bagussr <bagusssyamsu@gmail.com>
2023-08-30 13:51:51 +0700

    Home pape

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-// @deno-types=@types/react
-import React from 'react';
+// @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
+import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './page/Home';
 import { MainLayout } from './layout/MainLayout';
+import { About } from './page/About';
 
-function App() {
+const App: FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainLayout />}>
             <Route path='' element={<Home />} />
+            <Route path='about' element={<About />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
--- vite.config.mts
+++ vite.config.mts
@@ -4,14 +4,20 @@ import react from 'npm:@vitejs/plugin-react@^4.0.0';
 
 import 'npm:react@^18.2.0';
 import 'npm:react-dom@^18.2.0/client';
-import 'npm:react-router-dom@^6.15.0';
 import 'npm:@chakra-ui/react@^2.8.0';
 import 'npm:@emotion/react@^11.7.1';
 import 'npm:@emotion/styled@^11.7.1';
 import 'npm:framer-motion@^10.16.0';
 import 'npm:react-icons@^4.3.1';
+import 'npm:react-router-dom@^6.15.0';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
